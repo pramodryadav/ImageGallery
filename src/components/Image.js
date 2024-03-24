639365b7e50eb35f4a8c34723fcffa8c2c2b48f5
pramodyadav <pramodryadavall@gmail.com>
2024-03-24 15:58:49 +0530

    changed style

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,19 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import img from './images/img.jpg'
 import img1 from './images/img1.jpg'
 import img2 from './images/img2.jpg'
 import img3 from './images/img3.jpg'
 function Image() {
-  let imgArray=[img,img1,img2,img3,img,img1,img2,img3];
-  const [srrc,setSrrc]=useState(img);
-  
+  let imgArray = [img, img1, img2, img3, img, img1, img2, img3,img, img1, img2, img3, img, img1, img2, img3];
+  const [srrc, setSrrc] = useState(img);
+
+  const imageContainerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (imageContainerRef.current) {
+      console.log("imageContainerRef.current",imageContainerRef.current);
+      imageContainerRef.current.scrollBy({
+        left: -450, // Adjust this value to control the scrolling speed
+        behavior: 'smooth',
+      });
+    }
+  };
+
+  const scrollRight = () => {
+    if (imageContainerRef.current) {
+      imageContainerRef.current.scrollBy({
+        left: 450, // Adjust this value to control the scrolling speed
+        behavior: 'smooth',
+      });
+    }
+  };
+
   return (
     <>
       <div className='ShowImage'>
-        <img src={srrc} alt="img"/>
+        <img src={srrc} alt="img" />
       </div>
-      <div className='imgContainer'>
-      {imgArray.map((src,index)=><img key={index} className='' src={src} alt="img"  onClick={(e)=>setSrrc(e.target.src)}/>)}
+      <div className='imgContainer' >
+        <button class="left scrollBtn" onClick={scrollLeft}>
+          <i class="fas fa-angle-double-left">&lt;</i>
+        </button>
+        <div className='scroll-images' ref={imageContainerRef}>
+          {imgArray.map((src, index) => <img
+            key={index}
+            className=''
+            src={src}
+            alt="img"
+            onClick={(e) => setSrrc(e.target.src)}
+          />)
+          }
+        </div>
+
+        <button class="right scrollBtn" onClick={scrollRight}>
+          <i class="fas fa-angle-double-right">&gt;</i>
+        </button>
       </div>
     </>
 
